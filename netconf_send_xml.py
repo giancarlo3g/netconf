701bfdc1c79ef9eefa674b547f4bd7c8c2f63345
Giancarlo Garcia <giancarlo3g@MacBook-Pro-de-Giancarlo.local>
2019-09-19 09:52:37 -0500

    new program to get port state from Nokia 7750 or VSR using Netconf

---
+++ netconf_send_xml.py
@@ -0,0 +1,72 @@
+from ncclient import manager
+import ncclient
+import xmltodict
+import xml.dom.minidom
+import sys
+from lxml import etree
+
+#def open_xml_request():
+    
+def open_connection(host,user,passwd):
+    return manager.connect(host=host,port=830,username=user,password=passwd,hostkey_verify=False,device_params={'name':'alu'})
+def write_xml(host,operation,data_xml):
+    print('writing xml reply...')
+    with open(f"{host}_{operation}.xml",'w') as f:
+        f.write(data_xml)
+def print_xml(xml_file):
+    t = xml.dom.minidom.parseString(str(xml_file)).toprettyxml()
+    formatted_xml = "".join([s for s in t.strip().splitlines(True) if s.strip()])
+    print(formatted_xml)
+def close_connection(m):
+    m.close_session()
+
+if __name__ == "__main__":
+
+    # enter connection info
+    host = str(input('host ip: '))
+    user = str(input('user: '))
+    passwd = str(input('password: '))
+
+    filter_interface_system='''
+    <filter type="subtree">
+      <configure xmlns="urn:nokia.com:sros:ns:yang:sr:conf">
+        <router>
+          <router-name>Base</router-name>
+          <interface>
+            <interface-name>system</interface-name>
+          </interface>
+        </router>
+      </configure>
+    </filter>
+    '''
+    filter_port='''
+    <filter type="subtree">
+      <state xmlns="urn:nokia.com:sros:ns:yang:sr:state">
+	    <port>
+          <port-id></port-id>
+          <oper-state></oper-state>
+          <type></type>
+          <statistics></statistics>
+        </port>
+      </state>
+    </filter> 
+    '''
+
+    #request_xml = str(sys.argv[1])
+    #operation = request_xml.split('.xml')[0]
+    #data = nc_session.get_config(filter_interface_system).data_xml
+
+    # parse xml file and send request via netconf
+    operation = 'running'
+    nc_session = open_connection(host,user,passwd)
+    data = nc_session.get(filter_port).data_xml
+    
+    # print and sava reply
+    write_xml(host,operation,data)
+    print_xml(data)
+
+    # close connection
+    close_connection(nc_session)
+    sys.exit(2)
+
+
